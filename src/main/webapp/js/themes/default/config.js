2447389baaaa74cf141744a6be20f779b1d3168d
Seth Mason <seth.k.mason@gmail.com>
2014-02-10 15:05:55 -0700

    .war file build

--- src/main/webapp/js/themes/default/config.js
+++
@@ -1,9 +0,0 @@
-var theme = theme || "default";
-var themeMap = 
-{
-	'*': {
-		// example overrides are provided here
-		//'views/AboutView' : 'themes/' + theme + '/views/AboutView.js',
-		//'templates/navbar.html' : 'themes/' + theme + '/templates/navbar.html'
-		}
-};
