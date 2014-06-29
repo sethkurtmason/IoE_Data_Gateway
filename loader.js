7ec00ecc8762e16dad1e4f221be189b5d786a618
Seth Mason <seth.k.mason@gmail.com>
2014-06-28 23:38:03 -0600

    Revert "no message"
    This reverts commit ef38815dbf609d6b53ccd9bdb1e73dcdf8200511.

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 var theme 		   = document.getElementById("loader").getAttribute("data-theme");
 var metacatContext = document.getElementById("loader").getAttribute("data-metacat-context");
 var mapKey 		   = document.getElementById("loader").getAttribute("data-map-key");
-if ((mapKey == "AIzaSyDH-qZoHlo-b4ayGVgqk0-IYUNThXFhqqY") || (!mapKey)) mapKey = null;
+if ((mapKey == "YOUR-GOOGLE-MAPS-API-KEY") || (!mapKey)) mapKey = null;
 
 // Step 2: let everything else be taken care of by the app
 preventCompatibilityIssues();
